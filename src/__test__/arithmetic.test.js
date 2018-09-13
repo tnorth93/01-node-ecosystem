441f6b5ae0be41e5bbf1bd136ed783f1420d02fe
Tom North <tnorth93@gmail.com>
2018-09-13 00:59:05 -0700

    finished testing

--- src/__test__/arithmetic.test.js
+++ src/__test__/arithmetic.test.js
@@ -1,8 +1,26 @@
 'use strict';
 
-describe('testing kind of often', () => {
-  test('testing test testers', () => {
-    const aNumber = 5;
-    expect(aNumber).toEqual(5);
+const arithmetic = require('../lib/arithmetic');
+
+describe('#arithmetic.test.js', () => {
+  describe('addition function', () => {
+    test('should return null in case either arity is not a number', () => {
+      const nanAnswer = arithmetic.add('yo', 'k');
+      expect(nanAnswer).toEqual(null);
+    });
+    test('should return the sum if both arity are numbers', () => {
+      const nanAnswer = arithmetic.add(7, 11);
+      expect(nanAnswer).toEqual(18);
+    });
+  });
+  describe('subtraction function', () => {
+    test('should return null in case either arity is not a number', () => {
+      const nanAnswer = arithmetic.sub('bye', 'bruh');
+      expect(nanAnswer).toEqual(null);
+    });
+    test('should return the difference if both arity are numbers', () => {
+      const nanAnswer = arithmetic.sub(23, 10);
+      expect(nanAnswer).toEqual(13);
+    });
   });
 });
